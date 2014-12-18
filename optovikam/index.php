04d047c452aed7c642c751e35ee8f72aa3462bf4
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2014-12-18 18:55:56 +0300

    It is ready for the main page

--- optovikam/index.php
+++ optovikam/index.php
@@ -1,8 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оптовикам");
-?>
+?>Text here....
 
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- urlrewrite.php
+++ urlrewrite.php
@@ -6,6 +6,12 @@ $arUrlRewrite = array(
 		"ID" => "bitrix:news",
 		"PATH" => "/news/index.php",
 	),
+	array(
+		"CONDITION" => "#^/#",
+		"RULE" => "",
+		"ID" => "bitrix:catalog",
+		"PATH" => "/index.php",
+	),
 );
 
 ?>
