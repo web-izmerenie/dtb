0983d5811ec3eb12f82f771c4cb4454ab043e3e4
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2014-12-29 16:42:03 +0300

    fix bug catalog index.php

--- katalog/index.php
+++ katalog/index.php
@@ -1,8 +1,9 @@
 <?if(!$_GET['SECTION_CODE']){
-   header ('Location: /');
-}?>
-<?define("SIDEBAR", "Y");?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+    header ('Location: /');
+    exit();
+}
+define("SIDEBAR", "Y");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Каталог");
 ?>
 <?$APPLICATION->IncludeFile("/katalog/component_call.php");?>
